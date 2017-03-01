740ee18f4ece3da96caf412614a135032ff0de3b
Luke Schlangen <lukeschlangen@gmail.com>
2017-03-01 08:30:20 -0600

    Adding additional columns

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,13 +10,38 @@ $(document).ready(function(){
         console.log('response', response);
         $('#bookShelf').empty();
         for (var i = 0; i < response.length; i++) {
-          $('#bookShelf').append('<li>Title: ' + response[i].title + ', Author: ' + response[i].author + '</li>');
+          var currentBook = response[i];
+          var $newBook = $('<tr>');
+          $newBook.append('<td>' + currentBook.title + '</td>');
+          $newBook.append('<td>' + currentBook.author + '</td>');
+          $newBook.append('<td>' + currentBook.edition + '</td>');
+          $newBook.append('<td>' + currentBook.publisher + '</td>');
+          $('#bookShelf').append($newBook);
         }
       }
     });
   }
 
-  $('#newBookButton').on('click', function(){
+  $('#newBookForm').on('submit', function(event){
+    event.preventDefault();
+    var newBookObject = {};
+    newBookObject.title = $('#newBookTitle').val();
+    newBookObject.author = $('#newBookAuthor').val();
+    newBookObject.edition = $('#newBookEdition').val();
+    newBookObject.publisher = $('#newBookPublisher').val();
+    $.ajax({
+      type: 'POST',
+      url: '/books/new',
+      data: newBookObject,
+      success: function(response){
+        console.log(response);
+        getBookData();
+        $('#newBookForm > input').val('');
+      }
+    });
+  });
+
+  $('#bookShelf').on('click', '.saveButton', function(){
     var newBookObject = {};
     newBookObject.title = $('#newBookTitle').val();
     newBookObject.author = $('#newBookAuthor').val();
